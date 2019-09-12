bd6751188dd31716f56b846163063a861aa72c7b
drbarq <J.Tustin@gmail.com>
2019-09-12 12:13:11 -0600

    adding in mongo

--- tracker-server/src/index.js
+++ tracker-server/src/index.js
@@ -1,6 +1,20 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
+
+const mongoUri = 'mongodb+srv://admin:passwordpassword@trackerapp-7uhha.mongodb.net/test?retryWrites=true&w=majority'
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+})
+mongoose.connection.on('connected', () => {
+    console.log('Connected to Mongo instance')
+})
+mongoose.connection.on('errror', (err) => {
+    console.error('Error connecting to Mongo', err)
+})
+
 
 app.get('/', (req, res) => {
     res.send('Hi There!')
