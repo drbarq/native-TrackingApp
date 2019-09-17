0f9d9d27b9eb3c0d1b4f06fdeccdf4bf019f1845
drbarq <J.Tustin@gmail.com>
2019-09-17 17:13:12 -0600

    adding in stlyes and login

--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -3,27 +3,45 @@ import { View, StyleSheet } from 'react-native'
 import { Text, Input, Button } from 'react-native-elements'
 import Spacer from '../components/Spacer'
 
-
 const SignupScreen = props => {
-    return <>
-        <Spacer>
-            <Text h3>Sign Up for Tracker</Text>
-        </Spacer>
-        <Input 
-            label="Email"
-        />
-        <Spacer />
-        <Input 
-            label="Password"
-        />
-        <Spacer>
-            <Button 
-                title="Sign Up"
+    return (
+        <View style={styles.container}>
+            <Spacer>
+                <Text h3>Sign Up for Tracker</Text>
+            </Spacer>
+            <Input 
+                label="Email"
             />
-        </Spacer>
-    </>
+            <Spacer />
+            <Input 
+                label="Password"
+            />
+            <Spacer>
+                <Button 
+                    title="Sign Up"
+                />
+            </Spacer>
+        </View>
+    )
 }
 
-const styles = StyleSheet.create({})
+// SignupScreen.navigationOptions = () => {
+//     return {
+//         header: null
+//     }
+// }
+// set it equal to a function to get access to navigation props
+
+SignupScreen.navigationOptions = {
+        header: null
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        marginBottom: 150
+    }
+})
 
 export default SignupScreen
