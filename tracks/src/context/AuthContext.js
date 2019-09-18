5b67ed62490ca8d9891c8fd24b35b489aba0b565
drbarq <J.Tustin@gmail.com>
2019-09-17 18:11:21 -0600

    Add in actions

--- tracks/src/context/AuthContext.js
+++ tracks/src/context/AuthContext.js
@@ -7,8 +7,30 @@ const authReducer = (state, action) => {
     }
 }
 
+const signup = dispatch => {
+    return ({ email, password }) => {
+        // make api request to sign up with that email and password
+        // if we sign up, modify the state and say we are authenticated 
+        // if sign up fails, notify the user with an error message
+    }
+}
+
+const signin = dispach => {
+    return ({ email, password }) => {
+        // try to sign in
+        // handle success by updating state 
+        // handle failure with error message
+    }
+}
+
+const signout = dispatch => {
+    return () => {
+        // somehow sign out 
+    }
+}
+
 export const { Context, Provider } = createDataContext(
     authReducer,
-    {},
+    {signin, signout, signup},
     { isSignedIn: false }
 )
