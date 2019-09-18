0b228410dd9ef473627d94b82b2ef97089344858
drbarq <J.Tustin@gmail.com>
2019-09-18 17:13:56 -0600

    adding in resolve auth

--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext } from 'react'
 import { View, StyleSheet } from 'react-native'
 import { Context as AuthContext} from '../context/AuthContext'
 import { NavigationEvents } from 'react-navigation'
@@ -7,11 +7,7 @@ import NavLink from '../components/NavLink'
 
 
 const SignupScreen = (props) => {
-    const { state, signup, clearErrorMessage, tryLocalSignin } = useContext(AuthContext)
-
-    useEffect(() => {
-        tryLocalSignin()
-    }, [])
+    const { state, signup, clearErrorMessage } = useContext(AuthContext)
 
     return (
         <View style={styles.container}>
