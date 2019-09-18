92859849f76aa4e568a35e1a3272cd11d0700edf
drbarq <J.Tustin@gmail.com>
2019-09-18 16:48:03 -0600

    Adding in error funcitonality

--- tracks/src/context/AuthContext.js
+++ tracks/src/context/AuthContext.js
@@ -9,9 +9,15 @@ const authReducer = (state, action) => {
             return { errorMessage: '', token: action.payload }
         case 'add_error':
             return { ...state, errorMessage: action.payload } 
+        case 'clear_error_message':
+            return { ...state, errorMessage: ''}
         default:
             return state
     }
+}
+
+const clearErrorMessage = dispach => () => {
+    dispach({type: 'clear_error_message' })
 }
 
 const signup = dispatch => {
@@ -55,6 +61,6 @@ const signout = dispatch => {
 
 export const { Context, Provider } = createDataContext(
     authReducer,
-    {signin, signout, signup},
+    {signin, signout, signup, clearErrorMessage},
     { token: null, errorMessage: '' }
 )
