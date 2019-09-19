d01e35950810f6e064191f67367221c1a3d6ff66
drbarq <J.Tustin@gmail.com>
2019-09-18 18:16:12 -0600

    Adding in map functionality

--- tracks/src/components/Map.js
+++ tracks/src/components/Map.js
@@ -1,19 +1,39 @@
 import React from 'react'
 import { Text, StyleSheet } from 'react-native'
-import MapView from 'react-native-maps'
+import MapView, { Polyline } from 'react-native-maps'
 
 const Map = () => {
+
+    let points = []
+    for (let i = 0; i < 20; i++) {
+        if (i % 2 === 0) {
+            points.push({
+                latitude: 39.769148 + i * 0.001,
+                longitude: -104.974713 + i * 0.001
+            })
+        } else {
+            points.push({
+                latitude: 39.769148 - i * 0.002,
+                longitude: -104.974713 - i * 0.001
+            })
+        }
+    }
+
     return (
         <MapView
             style={styles.map}
             initialRegion={{
-                latitude: 37.33233,
-                longitude: -122.03121,
+                latitude: 39.769148,
+                longitude: -104.974713,
                 latitudeDelta: 0.01,
                 longitudeDelta: 0.01
 
             }}
-        />
+        >
+            <Polyline 
+                coordinates={points}
+            />
+        </MapView>
     )
 }
 
