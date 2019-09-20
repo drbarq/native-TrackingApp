1ad56f709ac5b482f5b7be8d8e31e4cc18a7a4eb
drbarq <J.Tustin@gmail.com>
2019-09-20 10:47:48 -0600

    adding in functionality and navigation

--- tracks/src/screens/TrackListScreen.js
+++ tracks/src/screens/TrackListScreen.js
@@ -1,21 +1,27 @@
 import React, {useContext} from 'react'
-import { View, StyleSheet, Text, Button } from 'react-native'
+import { View, StyleSheet, Text, FlatList, TouchableOpacity} from 'react-native'
 import { NavigationEvents } from 'react-navigation'
 import { Context as TrackContext } from '../context/TrackContext'
+import { ListItem } from 'react-native-elements'
 
 
 const TrackListScreen = props => {
     const { state, fetchTracks } = useContext(TrackContext)
 
-    console.log(state)
-
     return (
         <>
-            <NavigationEvents onWillFocus={() => {fetchTracks}} />
+            <NavigationEvents onWillFocus={fetchTracks} />
             <Text style={{fontSize: 48}}>Track List Screen</Text>
-            <Button 
-                title="Go to Track Detail"
-                onPress={() => props.navigation.navigate('TrackDetail')}
+            <FlatList 
+                data={state}
+                keyExtractor={item => item._id}
+                renderItem={({ item }) => {
+                    return (
+                        <TouchableOpacity>
+                            <ListItem chevron title={item.name}/>
+                        </TouchableOpacity>
+                    )
+                }}
             />
         </>
     )
@@ -24,3 +30,9 @@ const TrackListScreen = props => {
 const styles = StyleSheet.create({})
 
 export default TrackListScreen
+
+
+// <Button 
+// title="Go to Track Detail"
+// onPress={() => props.navigation.navigate('TrackDetail')}
+// />
