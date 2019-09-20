5aacd59251c62a07a6781c77bdc155e99e10b906
drbarq <J.Tustin@gmail.com>
2019-09-20 10:30:36 -0600

    adding in get

--- tracks/src/screens/TrackListScreen.js
+++ tracks/src/screens/TrackListScreen.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import { View, StyleSheet, Text, Button } from 'react-native'
+import { NavigationEvents } from 'react-navigation'
+import { Context as TrackContext } from '../context/TrackContext'
 
 
 const TrackListScreen = props => {
+    const { state, fetchTracks } = useContext(TrackContext)
+
+    console.log(state)
+
     return (
         <>
+            <NavigationEvents onWillFocus={() => {fetchTracks}} />
             <Text style={{fontSize: 48}}>Track List Screen</Text>
             <Button 
                 title="Go to Track Detail"
