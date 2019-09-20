cb6ca9b0e8a342c2c425278bec24a1398c55c3ee
drbarq <J.Tustin@gmail.com>
2019-09-20 10:22:42 -0600

    adding in navigate and post requests

--- tracks/src/context/LocationContext.js
+++ tracks/src/context/LocationContext.js
@@ -12,6 +12,8 @@ const locationReducer = (state, action) => {
             return { ...state, recording: true}
         case "add_current_location":
             return {...state, currentLocation: action.payload } 
+        case "reset":
+            return {...state, name: '', locations: [] }
         default:
             return state
     }
@@ -32,9 +34,12 @@ const addLocation = dispatch => (location, recording) => {
         dispatch({type: 'add_location', payload: location})
     }
 }
+const reset = dispatch => () => {
+    dispatch({type: 'reset'})
+}
 
 export const { Context, Provider } = createDataContext(
     locationReducer,
-    { startRecording, stopRecording, addLocation, changeName },
+    { startRecording, stopRecording, addLocation, changeName, reset },
     {name: '', recording: false, locations: [], currentLocation: null}
 )
